ac8d0d5bd56eb482074dc3f4f7b69c851ca97590
Kim eunji <dossiba0604@gmail.com>
2023-01-27 00:10:16 +0900

    usecontext추가

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
-import Youtube from "../api/youtube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new Youtube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
 
   return (
     <>
