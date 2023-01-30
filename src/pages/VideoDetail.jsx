b82a97258be0a86168b1d1eb198fca49380a76d4
Kim eunji <dossiba0604@gmail.com>
2023-01-30 14:01:09 +0900

    css적용

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function VideoDetail() {
+  // const { videoId } = useParams();
+  const { state } = useLocation();
+  console.log(state);
   return <div>VideoDetail</div>;
 }
